4e699d82f3585da7015abe0f9be21b70d13831f8
1mishra <shivank.mishra@gmail.com>
2017-12-14 22:46:18 -0600

    changed client

--- client.py
+++ client.py
@@ -1,20 +1,26 @@
 from Server import Server
+import client
+import random
+import math
+from numpy import zeros
+from scipy.stats import norm
+from six.moves import input
+import matplotlib.pyplot as plt
 
 def main():
     # option_type = str(input("Enter 'call' for call options or 'put' for put options: "))
-    expiry = float(input("Enter the expiry of the option (Total days for option): "))
-    strike = float(input("Enter the strike price of the option (The price you are willing to pay for a future): "))
-    spot = float(input("Enter the spot value of the option (Stock price currently): "))
+    expiry = float(input("Enter the expiry of the option: "))
+    strike = float(input("Enter the strike price of the option: "))
+    spot = float(input("Enter the spot value of the option: "))
     volatility = float(input("Enter the volatility value of the option as a rate: ")) / 100
     interest_rate = float(input("Enter the risk free interest rate as a rate: ")) / 100
-    mean = float(input("Enter the mean value: "))
     number_of_paths = int(input("Enter the number of paths you want to run the simulation for: "))
-    call = VanillaOption(expiry, strike, spot, volatility, interest_rate, number_of_paths)
+    call = Server(expiry, strike, spot, volatility, interest_rate, number_of_paths)
     print("Monte Carlo Result: ", call.monte_carlo_pricer())
     print("Black Scholes Model: ", call.black_scholes())
-    # call.visulization()
+    # print("-----: ", call.Price_calculation())
+
+
 
 if __name__ == '__main__':
     main()
-else:
-    print("run from import")
